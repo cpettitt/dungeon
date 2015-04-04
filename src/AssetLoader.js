0241f7a3338bf9397364309223d3bbca76b758bd
Chris Pettitt <cpettitt@gmail.com>
2015-04-03 18:18:20 -0700

    Initial asset loader and static render of boy sprite

---
+++ src/AssetLoader.js
@@ -0,0 +1,21 @@
+var PIXI = require("pixi.js");
+
+class AssetLoader {
+    loadAssets(assets, callback) {
+        const loader = new PIXI.AssetLoader(assets, false);
+        const results = {};
+
+        loader.on("progress", function(event) {
+            if (!(event.data.loader instanceof PIXI.JsonLoader)) {
+                return;
+            }
+
+            const eventLoader = event.data.loader;
+            results[eventLoader.url] = eventLoader.json;
+        });
+        loader.on("onComplete", function() { callback(results); });
+        loader.load();
+    }
+}
+
+module.exports = AssetLoader;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,30 @@
 // Force loading of the babel polyfill.
 require("babelify/polyfill");
 
+const PIXI = require("pixi.js");
+const AssetLoader = require("./AssetLoader");
+
+const renderer = new PIXI.autoDetectRenderer(800, 600);
+const stage = new PIXI.Stage();
+
+document.body.appendChild(renderer.view);
+
+const assets = [
+    "img/boy.json",
+    "img/boy.png"
+];
+new AssetLoader().loadAssets(assets, () => {
+    console.log("Assets loaded!");
+
+    const boySprite = PIXI.Sprite.fromFrame("boy-down1");
+    stage.addChild(boySprite);
+
+    requestAnimationFrame(loop);
+});
+
+function loop() {
+    renderer.render(stage);
+    requestAnimationFrame(loop);
+}
+
 console.log("Bundle loaded!");
